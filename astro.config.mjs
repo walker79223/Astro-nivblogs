4d1c5caa1ca05eacb8dcc9fb04bd87364b7737f2
W47K3R <puravmehta47@gmail.com>
2022-08-24 18:06:07 +0530

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte'
+import mdx from '@astrojs/mdx'
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://nivblogs.com',
+  integrations: [mdx(), svelte()],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord',
+    },
+    rehypePlugins: [
+      [
+        'rehype-external-links',
+        {
+          target: '_blank',
+        },
+      ],
+    ],
+  },
+})
